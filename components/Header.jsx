1098e42a365875f41908fe6b4a98261117ecf0fd
pierrek6e <pierrecass75@gmail.com>
2022-08-17 12:52:35 +0700

    improve Header Item component

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,13 @@ import { HomeIcon, BadgeCheckIcon, CollectionIcon, LightningBoltIcon, SearchIcon
 
 const Header = () => {
   return (
-    <header className='flex justify-between'>
-    <div className="flex justify-evenly w-[400px] ">
+    <header className='flex justify-between flex-col sm:flex-row m-5'>
+    <div className="flex flex-grow justify-evenly max-w-2xl ">
         <HeaderItem title="HOME" Icon={<HomeIcon className='h-8 mb-1'/>} />
-        <HeaderItem title="" icon=""/>
-        <HeaderItem title="" icon=""/>
-        <HeaderItem title="" icon=""/>
+        <HeaderItem title="TRENDING" Icon={<LightningBoltIcon className='h-8 mb-1'/> }/>
+        <HeaderItem title="VERIFIED" Icon={<BadgeCheckIcon className='h-8 mb-1'/>} />
+        <HeaderItem title="SEARCH" Icon={<SearchIcon className='h-8 mb-1'/>} />
+        <HeaderItem title="ACCOUNT" Icon={<UserCircleIcon className='h-8 mb-1'/>} />
     </div>
 
       <Image
--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const HeaderItem = ({ title, Icon }) => {
   return (
-    <div className='group flex flex-col items-center cursor-pointer w-12 sm:w-20 border-2 border-red-500'>
-      {Icon}
-      <p className='opacity-20 group-hover:opacity-100 tracking-widest'>
+    <div className='group flex flex-col items-center cursor-pointerhover:text-white w-12 sm:w-20'>
+      <div className="group-hover:animate-bounce">{Icon}</div>
+      <p className='opacity-30 group-hover:opacity-100 tracking-widest'>
         {title}
       </p>
     </div>

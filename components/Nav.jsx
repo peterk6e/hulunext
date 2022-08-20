5e9d7f1cd73c911bf2d486d1b9326ba64425f45c
pierrek6e <pierrecass75@gmail.com>
2022-08-20 18:56:02 +0700

    add fade on nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,12 +3,17 @@ import requests from "../utils/requests"
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className='relative'>
       <div className='flex px-10 h-12 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll'>
         {Object.entries(requests).map(([key, { title, url }]) => (
-          <p className="last:pr-24 cursor-pointer duration-200 transform hover:scale-125 hover:text-white active:text-orange-300" key={key}>{title}</p>
+          <p
+            className='last:pr-24 cursor-pointer duration-200 transform hover:scale-125 hover:text-white active:text-orange-300'
+            key={key}>
+            {title}
+          </p>
         ))}
       </div>
+      <div className='absolute top-0 right-0 bg-gradient-to-l from-[#000333] h-12 w-1/12' />
     </nav>
   )
 }

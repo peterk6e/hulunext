84e2066c64cd709f666086d2409de09e202f7f6a
pierrek6e <pierrecass75@gmail.com>
2022-08-21 21:20:45 +0700

    display movies

--- components/Movies.jsx
+++ components/Movies.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Thumbnail from './Thumbnail'
+import React from "react"
+import Thumbnail from "./Thumbnail"
+import FlipMove from "react-flip-move"
 
-const Movies = ({movies}) => {
+const Movies = ({ movies }) => {
   return (
-    <div>
-        {movies?.map((movie)=>(
-            <Thumbnail key={movie.id} movie={movie} />
-        ))}
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
+      {movies?.map(movie => (
+        <Thumbnail key={movie.id} movie={movie} />
+      ))}
     </div>
   )
 }
--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -1,19 +1,26 @@
 import React from "react"
 import Image from "next/image"
+import { ThumbUpIcon } from "@heroicons/react/outline"
 
 const Thumbnail = ({ movie }) => {
-  const BASE_URL = 'https://image.tmdb.org/t/p/original'
+  const BASE_URL = "https://image.tmdb.org/t/p/original"
 
   return (
-    <div className=''>
+    <div className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout='responsive'
         src={`${BASE_URL}${movie.poster_path}`}
-        height={1080}
-        width={1920}
+        height={540}
+        width={960}
       />
-      <div className="p-2">
-        <p className="truncate max-w-md">{movie.overview}</p>
+      <div className='p-2'>
+        <p className='truncate max-w-md'>{movie.overview}</p>
+        <p className='text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{movie.title || movie.original_name}</p>
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
+          {movie.media_type && `${movie.media_type} • `}{" "}
+          {movie.release_date || movie.first_air_date} •{" "}
+          <ThumbUpIcon className='h-5 mx-2' /> {movie.vote_count}
+        </p>
       </div>
     </div>
   )
